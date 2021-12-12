6f3edd60086a1c8abd9367ab8f6058ab892e2df2
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2021-12-12 22:08:19 +0700

    Delete do_an directory

--- do_an/admin/add_search.php
+++
@@ -1,4 +0,0 @@
-<?php
-$tim_kiem = $_GET['tim_kiem'];
-$link = $_GET['link'];
-header("location:admin_view.php?link=$link&tim_kiem=$tim_kiem");
--- do_an/root/connect.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-	$ket_noi = mysqli_connect('localhost','root','','bitis');
-	mysqli_set_charset($ket_noi,'utf8');
