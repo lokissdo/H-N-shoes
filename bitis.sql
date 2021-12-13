8bfd9deb1de59d3abb2371cd0858aae52ef67e23
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2021-12-13 09:35:14 +0700

    Update bitis.sql

--- bitis.sql
+++ bitis.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Dec 13, 2021 at 01:24 AM
--- Server version: 5.7.33
--- PHP Version: 7.4.19
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
