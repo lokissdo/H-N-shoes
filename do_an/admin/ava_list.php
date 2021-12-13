b8d86919a6535dd3938962042aa7826728cc3e8b
Hung <dokhaihung2003@gmail.com>
2021-12-13 12:13:20 +0700

    body

---
+++ do_an/admin/ava_list.php
@@ -0,0 +1,12 @@
+		<div id="main_body">
+			<div class="items"></div>
+			<div class="items"></div>
+			<div class="items"></div>
+			<div class="items"></div>
+			<div class="items"></div>
+			<div class="items"></div>
+			<div class="items"></div>
+			<div class="items"></div>
+
+			<div id="page"></div>
+		</div>
---
+++ do_an/admin/detail_list.php
@@ -0,0 +1,12 @@
+		<div id="main_list">
+			<div class="list_items"></div>
+			<div class="list_items"></div>
+			<div class="list_items"></div>
+			<div class="list_items"></div>
+			<div class="list_items"></div>
+			<div class="list_items"></div>
+			<div class="list_items"></div>
+			<div class="list_items"></div>
+
+			<div id="page_list"></div>
+		</div>
