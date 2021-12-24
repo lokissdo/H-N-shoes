95f79cb53b85ad1a0269713ab2b1ae2c98fc24d6
Hưng <88534292+DoKhaiHung@users.noreply.github.com>
2021-12-24 11:38:22 +0700

    add table: products_gender, products_category

--- bitis.sql
+++ bitis.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 20, 2021 at 03:54 AM
+-- Generation Time: Dec 24, 2021 at 04:32 AM
 -- Server version: 5.7.33
 -- PHP Version: 7.4.19
 
@@ -45,7 +45,7 @@ CREATE TABLE `cli_list` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `gender` bit(2) NOT NULL,
-  `address` varchar(200) NOT NULL,
+  `address` varchar(200) DEFAULT NULL,
   `email` varchar(200) NOT NULL,
   `phone` varchar(15) NOT NULL,
   `photo` varchar(200) DEFAULT NULL,
@@ -59,10 +59,13 @@ CREATE TABLE `cli_list` (
 --
 
 INSERT INTO `cli_list` (`id`, `name`, `gender`, `address`, `email`, `phone`, `photo`, `password`, `birthday`, `token`) VALUES
-(1, 'hung', b'01', '12', '12', '12', NULL, '', NULL, NULL),
-(2, 'hung', b'01', '12', '12', '12', NULL, '', NULL, NULL),
-(3, '1', b'00', 'no', 'dokhaihung2003@gmail.com', '1', 'no', '$2y$10$39P/ctVOL53o4KOVxcUWu.pWBhsrrWpxgI.RrNKl0IWRkBA1U11.C', NULL, '1dee192b07f289b6ec2837cf691c17fe1639971388'),
-(4, '12', b'01', 'no', 'dokhaihung2003@gmail.com2222', '1', 'no', '$2y$10$15PoIrYmtFjoD/9rAOVmJeb7b2DpNW4rU81LrcJOYQs58JqbcKVju', '2021-12-25', NULL);
+(4, '12', b'01', 'no', 'dokhaihung2003@gmail.com2222', '1', 'no', '$2y$10$15PoIrYmtFjoD/9rAOVmJeb7b2DpNW4rU81LrcJOYQs58JqbcKVju', '2021-12-25', NULL),
+(5, 'Lokiss', b'00', 'no', '2@2', '12', 'no', '$2y$10$ktGfaSY4oxpCqfSr3Q7qa.MT9WgGq2gl47z80jzAPVJ.kwSGzznZS', '2021-12-10', '695bcb9f1897e98235f64704a06753b31640178666'),
+(9, '2', b'00', '', '2@34', '2', '', '$2y$10$I9Ttuye/k3EYydgDY425rO0ZXSbe0OcLA63gSdhrzrloNGBRfE/P2', '2021-12-25', NULL),
+(10, '22', b'00', '', 'dokhaihung2003@gmail.com11', '2', '', '$2y$10$vT8fLQ1.0KM/6dTFwN6SwOAc9UaMH3Y694Ob0luSs0rrEGPEEIs2.', NULL, NULL),
+(11, '23', b'00', '', '1@111', '2', '', '$2y$10$LHVNhnIAwRcbtr3rI4YzWO41w80BoRJX6dGJqb.ScB1AR1Ig/Qg2W', NULL, NULL),
+(12, '2', b'01', '', '22@3', '2', '', '$2y$10$luJ2l6oUgNjRUxPsP8597u0omxHR5hlph1E521LU9m1DlZ1t4jMcK', '2021-12-21', NULL),
+(13, '1', b'01', '', 'dokhaihung2003@gmail.com', '1', '', '$2y$10$jM8Mi22bdRg4XLdJaomTBuXOgvX9mOOkTjUMQGQe4pDoy3BwKWBna', NULL, 'd5d44c5c39f71300c70b9b8649c347121640254101');
 
 -- --------------------------------------------------------
 
@@ -106,6 +109,47 @@ CREATE TABLE `out_list` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `products_category`
+--
+
+CREATE TABLE `products_category` (
+  `id` smallint(11) NOT NULL,
+  `category` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `products_category`
+--
+
+INSERT INTO `products_category` (`id`, `category`) VALUES
+(1, 'Giày Sandal'),
+(2, 'Giày Tây'),
+(3, 'Giày Thể Thao');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products_gender`
+--
+
+CREATE TABLE `products_gender` (
+  `id` tinyint(11) NOT NULL,
+  `gender` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `products_gender`
+--
+
+INSERT INTO `products_gender` (`id`, `gender`) VALUES
+(1, 'Nam'),
+(2, 'Nữ'),
+(3, 'Nam, Nữ'),
+(4, 'Trẻ em');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `products_list`
 --
 
@@ -114,30 +158,30 @@ CREATE TABLE `products_list` (
   `name` varchar(200) NOT NULL,
   `price` int(11) NOT NULL,
   `quantity` int(11) DEFAULT NULL,
-  `category` varchar(50) DEFAULT NULL,
+  `gender_id` tinyint(4) NOT NULL,
+  `category_id` smallint(6) NOT NULL,
   `descrpition` text NOT NULL,
   `photo` varchar(200) NOT NULL,
-  `manufacturers_id` int(11) NOT NULL,
-  `gender` int(11) DEFAULT NULL
+  `manufacturers_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `products_list`
 --
 
-INSERT INTO `products_list` (`id`, `name`, `price`, `quantity`, `category`, `descrpition`, `photo`, `manufacturers_id`, `gender`) VALUES
-(2, 'Run Star Hike Create Next Purpose: Sustainability Hi-Top\r\n', 2500000, 1000, 'Giày thể thao', '', 'https://www.converse.com.vn/pictures/catalog/products/sneakers/2021/ctas/171575/171575Cstandard.jpg', 1, 1),
-(5, 'Chuck Taylor All Star Lift Surface Fusion', 1500000, 1000, 'Giày thể thao', '', 'https://www.converse.com.vn/pictures/catalog/products/sneakers/2021/ctas/571670c/571670Cstandard.jpg', 1, 0),
-(6, 'Chuck Taylor All Star Crater Knit High Top', 1500000, 1000, 'Giày thể thao', '', 'https://www.converse.com.vn/pictures/catalog/products/sneakers/2021/ctas/170869c/170869Cshot2.jpg', 1, 3),
-(7, 'Giày sandal nữ Aokang 682831037', 150000, 20, 'Giày Sandal', '', 'https://img.yes24.vn/Upload/ProductImage/anvietsh/2013660_L.jpg?width=550&height=550', 4, 0),
-(8, 'Giày sandal nữ Senta SE107SH08YOHVN', 1500000, 1000, 'Giày Sandal', '', 'https://img.yes24.vn/Upload/ProductImage/SENTA/1986228_L.jpg?width=550&height=550', 3, 0),
-(9, 'Giày sandal nữ Aokang 682831080', 100000, 200, 'Giày Sandal', '', 'https://img.yes24.vn/Upload/ProductImage/anvietsh/2013639_L.jpg?width=550&height=550', 1, 0),
-(10, 'Giày sandal Adidas Adilete F35417', 1500000, 10009, 'Giày Sandal', '', 'https://img.yes24.vn/Upload/ProductImage/GmarketSport/2030428_L.jpg?width=550&height=550', 4, 1),
-(11, 'Giày Sandal Nam Aokang 182831014', 170000, 202, 'Giày Sandal', '', 'https://shop-cdn.coccoc.com/images/product/Hg/Cf/HgCfycRike.jpg', 4, 1),
-(12, 'Giày Sandal Doctor Nam BIGGBEN Da Bò Thật SD70', 200000, 999, 'Giày Sandal', '', 'https://salt.tikicdn.com/cache/400x400/ts/product/74/a2/d3/f02e8c1714cf99b458cba8269bb6d232.jpg.webp', 1, 1),
-(13, 'Sandal Eva Phun Nam Biti\'s Hunter DEMH00201XAM (Xám)', 1500000, 1000, 'Giày Sandal', '', 'https://salt.tikicdn.com/cache/400x400/ts/product/d5/1f/b6/5fb3c22b35097a46a7f46f436bbbc2e3.jpg.webp', 3, 1),
-(14, 'Giày Thể Thao Cao Cấp Nữ Biti\'s Hunter Layered Upper DSWH02800HOG (Hồng)', 15000009, 999, 'Giày thể thao', '', 'https://product.hstatic.net/1000230642/product/02800hog__2__dd6e1a064a294e108c2c90e7a728470d_1024x1024.jpg', 3, 0),
-(15, 'Giày Tây Nam Biti\'s DVM283770NAD (Nâu)', 150000, 999, 'Giày Tây', '', 'https://product.hstatic.net/1000230642/product/dvm283770nad_dd6947c86c3049a99227e2854ddc1ef0_1024x1024.jpg', 3, 1);
+INSERT INTO `products_list` (`id`, `name`, `price`, `quantity`, `gender_id`, `category_id`, `descrpition`, `photo`, `manufacturers_id`) VALUES
+(2, 'Run Star Hike Create Next Purpose: Sustainability Hi-Top\r\n', 2500000, 1000, 3, 3, '', 'https://www.converse.com.vn/pictures/catalog/products/sneakers/2021/ctas/171575/171575Cstandard.jpg', 1),
+(5, 'Chuck Taylor All Star Lift Surface Fusion', 1500000, 1000, 3, 3, '', 'https://www.converse.com.vn/pictures/catalog/products/sneakers/2021/ctas/571670c/571670Cstandard.jpg', 1),
+(6, 'Chuck Taylor All Star Crater Knit High Top', 1500000, 1000, 4, 3, '', 'https://www.converse.com.vn/pictures/catalog/products/sneakers/2021/ctas/170869c/170869Cshot2.jpg', 1),
+(7, 'Giày sandal nữ Aokang 682831037', 150000, 20, 2, 1, '', 'https://img.yes24.vn/Upload/ProductImage/anvietsh/2013660_L.jpg?width=550&height=550', 4),
+(8, 'Giày sandal nữ Senta SE107SH08YOHVN', 1500000, 1000, 2, 1, '', 'https://img.yes24.vn/Upload/ProductImage/SENTA/1986228_L.jpg?width=550&height=550', 3),
+(9, 'Giày sandal nữ Aokang 682831080', 100000, 200, 2, 1, '', 'https://img.yes24.vn/Upload/ProductImage/anvietsh/2013639_L.jpg?width=550&height=550', 1),
+(10, 'Giày sandal Adidas Adilete F35417', 1500000, 10009, 1, 1, '', 'https://img.yes24.vn/Upload/ProductImage/GmarketSport/2030428_L.jpg?width=550&height=550', 4),
+(11, 'Giày Sandal Nam Aokang 182831014', 170000, 202, 1, 1, '', 'https://shop-cdn.coccoc.com/images/product/Hg/Cf/HgCfycRike.jpg', 4),
+(12, 'Giày Sandal Doctor Nam BIGGBEN Da Bò Thật SD70', 200000, 999, 1, 1, '', 'https://salt.tikicdn.com/cache/400x400/ts/product/74/a2/d3/f02e8c1714cf99b458cba8269bb6d232.jpg.webp', 1),
+(13, 'Sandal Eva Phun Nam Biti\'s Hunter DEMH00201XAM (Xám)', 1500000, 1000, 1, 1, '', 'https://salt.tikicdn.com/cache/400x400/ts/product/d5/1f/b6/5fb3c22b35097a46a7f46f436bbbc2e3.jpg.webp', 3),
+(14, 'Giày Thể Thao Cao Cấp Nữ Biti\'s Hunter Layered Upper DSWH02800HOG (Hồng)', 15000009, 999, 2, 3, '', 'https://product.hstatic.net/1000230642/product/02800hog__2__dd6e1a064a294e108c2c90e7a728470d_1024x1024.jpg', 3),
+(15, 'Giày Tây Nam Biti\'s DVM283770NAD (Nâu)', 150000, 999, 1, 2, '', 'https://product.hstatic.net/1000230642/product/dvm283770nad_dd6947c86c3049a99227e2854ddc1ef0_1024x1024.jpg', 3);
 
 --
 -- Indexes for dumped tables
@@ -164,11 +208,25 @@ ALTER TABLE `out_list`
   ADD KEY `FK_out_products_id` (`products_id`);
 
 --
+-- Indexes for table `products_category`
+--
+ALTER TABLE `products_category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products_gender`
+--
+ALTER TABLE `products_gender`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `products_list`
 --
 ALTER TABLE `products_list`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `FK_products_manufacturers_id` (`manufacturers_id`);
+  ADD KEY `FK_products_manufacturers_id` (`manufacturers_id`),
+  ADD KEY `FK_products_gender` (`gender_id`),
+  ADD KEY `FK_products_category` (`category_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -178,7 +236,7 @@ ALTER TABLE `products_list`
 -- AUTO_INCREMENT for table `cli_list`
 --
 ALTER TABLE `cli_list`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `manufactures`
@@ -191,6 +249,18 @@ ALTER TABLE `manufactures`
 --
 ALTER TABLE `out_list`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `products_category`
+--
+ALTER TABLE `products_category`
+  MODIFY `id` smallint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `products_gender`
+--
+ALTER TABLE `products_gender`
+  MODIFY `id` tinyint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `products_list`
@@ -213,6 +283,8 @@ ALTER TABLE `out_list`
 -- Constraints for table `products_list`
 --
 ALTER TABLE `products_list`
+  ADD CONSTRAINT `FK_products_category` FOREIGN KEY (`category_id`) REFERENCES `products_category` (`id`),
+  ADD CONSTRAINT `FK_products_gender` FOREIGN KEY (`gender_id`) REFERENCES `products_gender` (`id`),
   ADD CONSTRAINT `FK_products_manufacturers_id` FOREIGN KEY (`manufacturers_id`) REFERENCES `manufactures` (`id`);
 COMMIT;
 
