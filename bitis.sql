8444e5c20e9e9c0103be0cfdc3e6d1c0e832905c
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2022-01-03 14:28:01 +0700

    Add files via upload

--- bitis.sql
+++ bitis.sql
@@ -1,4 +1,11 @@
-
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jan 03, 2022 at 07:27 AM
+-- Server version: 5.7.33
+-- PHP Version: 7.4.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -26,24 +33,26 @@ CREATE TABLE IF NOT EXISTS `adm_list` (
   `name` varchar(50) NOT NULL,
   `phone` varchar(15) NOT NULL,
   `address` varchar(100) NOT NULL,
-  `gender` bit(2) NOT NULL,
+  `gender` bit(1) NOT NULL,
   `birthday` date NOT NULL,
   `email` varchar(100) NOT NULL,
   `photo` varchar(200) NOT NULL,
   `password` varchar(200) NOT NULL,
-  `access` bit(2) NOT NULL,
+  `access` bit(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
-
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `adm_list`
 --
 
 INSERT INTO `adm_list` (`id`, `name`, `phone`, `address`, `gender`, `birthday`, `email`, `photo`, `password`, `access`) VALUES
-(1, 'Nhat', '09771168123', 'Điện Biên Phủ, Quận 1', b'01', '1997-09-23', 'red@gmail.com', 'https://static.wikia.nocookie.net/heroes-and-villians/images/8/83/Winnie_the_Pooh.png/revision/latest/scale-to-width-down/1000?cb=20191229203055', 'abc', b'01'),
-(3, 'Huy', '0982224466', 'Hải Phòng', b'01', '1998-07-23', 'blue@gmail.com', '//upload.wikimedia.org/wikipedia/vi/thumb/1/1a/Mickey-Mouse.jpg/220px-Mickey-Mouse.jpg', 'abc', b'00'),
-(5, 'Hùng', '0977227990', 'Vũng Tàu', b'01', '1991-02-14', 'yellow@gmail.com', 'https://bigcedar.com/wp-content/uploads/2020/12/AdobeStock_453941794-1.jpg', 'abc', b'00');
+(1, 'Nhat', '09771168123', 'Điện Biên Phủ, Quận 1', b'1', '1997-09-23', 'red@gmail.com', 'https://lumiere-a.akamaihd.net/v1/images/c94eed56a5e84479a2939c9172434567c0147d4f.jpeg?region=0,0,600,600', 'abc', b'1'),
+(3, 'Huy', '0982224466', 'Hải Phòng', b'1', '1998-07-23', 'blue@gmail.com', '//upload.wikimedia.org/wikipedia/vi/thumb/1/1a/Mickey-Mouse.jpg/220px-Mickey-Mouse.jpg', 'abc', b'0'),
+(5, 'Hùng', '0977227990', 'Vũng Tàu', b'1', '1991-02-14', 'yellow@gmail.com', 'https://bigcedar.com/wp-content/uploads/2020/12/AdobeStock_453941794-1.jpg', 'abc', b'0'),
+(6, 'Hương', '0933445566', 'Lâm Đồng', b'0', '1977-06-05', 'pink@gmail.com', 'https://static.wikia.nocookie.net/disney/images/a/a3/Minnie_Mouse_transparent.png/revision/latest?cb=20210124014343', 'abc', b'1'),
+(8, 'Bùi Xuân Bảo', '0988667981', '338 Hai Bà Trưng, Phường 8, 1, Thành phố Hồ Chí Minh', b'1', '1987-03-20', 'green@gmail.com', 'https://cafebiz.cafebizcdn.vn/thumb_w/600/2019/photo1557224364584-1557224364877-crop-15572243735951912688033.jpg', 'abc', b'1'),
+(9, 'Đặng Thanh Phương', '0979667790', '285 Hai Bà Trưng, Phường 8, Quận 3, Thành phố Hồ Chí Minh', b'0', '1992-07-08', 'teal@gmail.com', 'https://pbs.twimg.com/media/ExMLCgWVoAQaBef?format=jpg&name=large', 'abc', b'0');
 
 -- --------------------------------------------------------
 
@@ -64,7 +73,8 @@ CREATE TABLE IF NOT EXISTS `cli_list` (
   `birthday` date DEFAULT NULL,
   `token` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4;
+
 --
 -- Dumping data for table `cli_list`
 --
@@ -89,7 +99,7 @@ CREATE TABLE IF NOT EXISTS `manufactures` (
   `photo` varchar(200) NOT NULL,
   `address` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `manufactures`
@@ -98,7 +108,8 @@ CREATE TABLE IF NOT EXISTS `manufactures` (
 INSERT INTO `manufactures` (`id`, `name`, `description`, `phone`, `photo`, `address`) VALUES
 (1, 'CONVERSE', '', '123', '', 'here'),
 (3, 'Bitis', '', '', '', ''),
-(4, 'Nike', '', '', '', '');
+(4, 'Nike', '', '', '', ''),
+(5, 'Adidas', 'Adidas là một nhà sản xuất dụng cụ thể thao của Đức, một thành viên của Adidas Group.', '0917221177', 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Adidas_Logo.svg/800px-Adidas_Logo.svg.png', '1 17A ST, Aeon Mall, Aeon Binh Tan, 700000, Ho Chi Minh, Việt Nam');
 
 -- --------------------------------------------------------
 
@@ -118,7 +129,6 @@ CREATE TABLE IF NOT EXISTS `out_list` (
   PRIMARY KEY (`id`),
   KEY `FK_out_manufacturers_id` (`client_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
-
 
 --
 -- Dumping data for table `out_list`
@@ -142,6 +152,7 @@ CREATE TABLE IF NOT EXISTS `out_product` (
   PRIMARY KEY (`out_id`,`product_id`),
   KEY `FK_product_id` (`product_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Dumping data for table `out_product`
 --
@@ -185,6 +196,7 @@ CREATE TABLE IF NOT EXISTS `products_gender` (
   `gender` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+
 --
 -- Dumping data for table `products_gender`
 --
@@ -216,14 +228,14 @@ CREATE TABLE IF NOT EXISTS `products_list` (
   KEY `FK_products_manufacturers_id` (`manufacturers_id`),
   KEY `FK_products_gender` (`gender_id`) USING BTREE,
   KEY `FK_products_category` (`category_id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `products_list`
 --
 
 INSERT INTO `products_list` (`id`, `name`, `price`, `quantity`, `gender_id`, `category_id`, `description`, `photo`, `manufacturers_id`) VALUES
-(2, 'Run Star Hike Create Next Purpose: Sustainability Hi-Top\r\n', 2500000, 1000, 3, 3, '', 'https://www.converse.com.vn/pictures/catalog/products/sneakers/2021/ctas/171575/171575Cstandard.jpg', 1),
+(2, 'Run Star Hike Create Next Purpose: Sustainability Hi-Top', 2500000, 1000, 3, 3, '', 'https://www.converse.com.vn/pictures/catalog/products/sneakers/2021/ctas/171575/171575Cstandard.jpg', 1),
 (5, 'Chuck Taylor All Star Lift Surface Fusion', 1500000, 1000, 3, 3, '', 'https://www.converse.com.vn/pictures/catalog/products/sneakers/2021/ctas/571670c/571670Cstandard.jpg', 1),
 (6, 'Chuck Taylor All Star Crater Knit High Top', 1500000, 1000, 4, 3, '', 'https://www.converse.com.vn/pictures/catalog/products/sneakers/2021/ctas/170869c/170869Cshot2.jpg', 1),
 (7, 'Giày sandal nữ Aokang 682831037', 150000, 20, 2, 1, '', 'https://img.yes24.vn/Upload/ProductImage/anvietsh/2013660_L.jpg?width=550&height=550', 4),
@@ -233,8 +245,10 @@ INSERT INTO `products_list` (`id`, `name`, `price`, `quantity`, `gender_id`, `ca
 (11, 'Giày Sandal Nam Aokang 182831014', 170000, 202, 1, 1, '', 'https://shop-cdn.coccoc.com/images/product/Hg/Cf/HgCfycRike.jpg', 4),
 (12, 'Giày Sandal Doctor Nam BIGGBEN Da Bò Thật SD70', 200000, 999, 1, 1, '', 'https://salt.tikicdn.com/cache/400x400/ts/product/74/a2/d3/f02e8c1714cf99b458cba8269bb6d232.jpg.webp', 1),
 (13, 'Sandal Eva Phun Nam Biti\'s Hunter DEMH00201XAM (Xám)', 1500000, 1000, 1, 1, '', 'https://salt.tikicdn.com/cache/400x400/ts/product/d5/1f/b6/5fb3c22b35097a46a7f46f436bbbc2e3.jpg.webp', 3),
-(14, 'Giày Thể Thao Cao Cấp Nữ Biti\'s Hunter Layered Upper DSWH02800HOG (Hồng)', 15000009, 999, 2, 3, '', 'https://product.hstatic.net/1000230642/product/02800hog__2__dd6e1a064a294e108c2c90e7a728470d_1024x1024.jpg', 3),
-(15, 'Giày Tây Nam Biti\'s DVM283770NAD (Nâu)', 150000, 999, 1, 2, '', 'https://product.hstatic.net/1000230642/product/dvm283770nad_dd6947c86c3049a99227e2854ddc1ef0_1024x1024.jpg', 3);
+(14, 'Giày Thể Thao Cao Cấp Nữ Biti', 15000009, 998, 2, 3, '', 'https://product.hstatic.net/1000230642/product/02800hog__2__dd6e1a064a294e108c2c90e7a728470d_1024x1024.jpg', 3),
+(15, 'Giày Tây Nam Biti\'s DVM283770NAD (Nâu)', 150000, 999, 1, 2, '', 'https://product.hstatic.net/1000230642/product/dvm283770nad_dd6947c86c3049a99227e2854ddc1ef0_1024x1024.jpg', 3),
+(16, 'ZX 10000 KRUSTY BURGER', 3100000, 1000, 3, 3, 'Giày thể thao năng động đầy màu sắc', 'https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/c59ed45d8b8c443481fcac7c01403b85_9366/ZX_10000_KRUSTY_BURGER_trang_H05783_01_standard.jpg', 5),
+(17, 'Giày Superstar 82', 3500000, 999, 1, 3, 'Giày thể thao sành điệu chất chơi', 'https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/8c3a61df8aea41d48b16ad6500b34711_9366/Giay_Superstar_82_DJen_GX3746_01_standard.jpg', 5);
 
 -- --------------------------------------------------------
 
