e81c5a96c3f31687c913f2da06a4661ebbe15965
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2022-01-10 15:10:45 +0700

    Add files via upload

--- do_an/admin/detail_view/admin_detail.php
+++ do_an/admin/detail_view/admin_detail.php
@@ -5,23 +5,27 @@
 	$result = mysqli_fetch_array(mysqli_query($ket_noi,$qry));
 
 if($result['access'] == 1){
+	echo "<div class = 'denied_page'>";
 	echo '<h1>Bạn không có quyền truy cập hồ sơ này</h1><br>';
 	echo "<button onclick='reload_page()'>Quay lại</button>" ;
+	echo "</div>";
 } else {
-echo 	"<img id='adm_photo' src=\"".$result['photo']."\"><br>";
-echo 	"<div>
-			Tên nhân viên: ".$result['name']."<br>";
-echo	"Giới tính: ";
+echo 	"<div id='info_page'>";
+echo 	"<img id='adm_photo' src=\"".$result['photo']."\">";
+echo 	"<div id='detail_info'>
+			<p>Tên nhân viên: ".$result['name']."</p>";
+echo	"<p>Giới tính: ";
 if ($result['gender'] == 1){
 	echo "Nam";
 } else {
 	echo "Nữ";
 }
-echo  "<br>";
-echo "Số điện thoại: ".$result['phone']."<br>";
-echo	"Địa chỉ: ".$result['address']."<br>";
-echo	"Sinh nhật: ".$result['birthday']."<br>";
+echo  "</p>";
+echo "<p>Số điện thoại: ".$result['phone']."</p>";
+echo	"<p>Địa chỉ: ".$result['address']."</p>";
+echo	"<p>Sinh nhật: ".$result['birthday']."</p>";
 echo "<button onclick=\"fix_page(".$id.")\">Sửa thông tin</button>";
+echo "</div>";
 echo "</div>";
 }
 mysqli_close($ket_noi);
