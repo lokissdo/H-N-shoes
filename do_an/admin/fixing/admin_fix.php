0d8cfefda5325926187dd276dcdbabfc5fcf8e4c
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2022-01-11 15:24:19 +0700

    Add files via upload

--- do_an/admin/fixing/admin_fix.php
+++ do_an/admin/fixing/admin_fix.php
@@ -6,11 +6,12 @@
 
 session_start();
 if ($_SESSION['access'] == 1){
-echo 	"<img id='adm_photo' src=\"".$result['photo']."\"><br>";
-echo 	"<form method=\"post\" action=\"update/admin_update.php\">
-			<input type='hidden' name='id' value='".$id."' >
-			Tên nhân viên: <input type='text' name='admin_name' value='".$result['name']."'><br>";
-echo	"Giới tính: ";
+echo 	"<div id='info_page'>";
+echo 	"<img id='adm_photo' src=\"".$result['photo']."\">";
+echo 	"<form method=\"post\" action=\"update/admin_update.php\" id='fix_info'>
+			<input type='hidden' name='id' value='".$id."' style='display:none;'>
+			<p>Tên nhân viên: </p><input type='text' name='admin_name' value='".$result['name']."'>";
+echo	"<p>Giới tính: <p>";
 echo 	"<select name='gender'>";
 if ($result['gender'] == 1){
 	echo "<option selected>Nam</option><option>Nữ</option>";
@@ -18,12 +19,12 @@ if ($result['gender'] == 1){
 	echo "<option>Nam</option><option selected>Nữ</option>";
 }
 echo "</select>";
-echo  "<br>";
-echo "Số điện thoại: <input type='text' name='phone' value='".$result['phone']."'><br>";
-echo	"Địa chỉ: <input type='text' name='address' value='".$result['address']."'><br>";
-echo	"Sinh nhật: <input type='text' name='birthday' value='".$result['birthday']."'><br>";
+echo "<p>Số điện thoại: </p><input type='text' name='phone' value='".$result['phone']."'>";
+echo	"<p>Địa chỉ: </p><input type='text' name='address' value='".$result['address']."'>";
+echo	"<p>Sinh nhật: </p><input type='text' name='birthday' value='".$result['birthday']."'>";
 echo "<button type='submit'>Sửa thông tin</button>";
 echo "</form>";
+echo "</div>";
 } else{
 	header('location:logout.php');
 }
