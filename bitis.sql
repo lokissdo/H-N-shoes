23c9cc85ebc540460c24e5263b399fb30ec66803
hung <dokhaihung2003@gmai.com>
2022-01-12 11:41:55 +0700

    .

--- bitis.sql
+++ bitis.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 31, 2021 at 06:39 AM
+-- Generation Time: Jan 12, 2022 at 04:37 AM
 -- Server version: 5.7.33
 -- PHP Version: 7.4.19
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `bitis`
 --
+CREATE DATABASE IF NOT EXISTS `bitis` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `bitis`;
 
 -- --------------------------------------------------------
 
@@ -27,26 +29,32 @@ SET time_zone = "+00:00";
 -- Table structure for table `adm_list`
 --
 
-CREATE TABLE `adm_list` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `adm_list`;
+CREATE TABLE IF NOT EXISTS `adm_list` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `phone` varchar(15) NOT NULL,
   `address` varchar(100) NOT NULL,
-  `gender` bit(2) NOT NULL,
+  `gender` bit(1) NOT NULL,
   `birthday` date NOT NULL,
   `email` varchar(100) NOT NULL,
   `photo` varchar(200) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `access` bit(2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `password` varchar(200) NOT NULL,
+  `access` bit(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `adm_list`
 --
 
 INSERT INTO `adm_list` (`id`, `name`, `phone`, `address`, `gender`, `birthday`, `email`, `photo`, `password`, `access`) VALUES
-(1, 'Nhat', '09771168123', 'Điện Biên Phủ, Quận 1', b'01', '1997-09-23', 'red@gmail.com', 'https://static.wikia.nocookie.net/heroes-and-villians/images/8/83/Winnie_the_Pooh.png/revision/latest/scale-to-width-down/1000?cb=20191229203055', 'abc', b'01'),
-(3, 'Huy', '0982224466', 'Hải Phòng', b'01', '1998-07-23', 'blue@gmail.com', '//upload.wikimedia.org/wikipedia/vi/thumb/1/1a/Mickey-Mouse.jpg/220px-Mickey-Mouse.jpg', 'abc', b'00');
+(1, 'Nhat', '09771168123', 'Điện Biên Phủ, Quận 1', b'1', '1997-09-23', 'red@gmail.com', 'https://lumiere-a.akamaihd.net/v1/images/c94eed56a5e84479a2939c9172434567c0147d4f.jpeg?region=0,0,600,600', 'abc', b'1'),
+(3, 'Huy', '0982224466', 'Hải Phòng', b'1', '1998-07-23', 'blue@gmail.com', '//upload.wikimedia.org/wikipedia/vi/thumb/1/1a/Mickey-Mouse.jpg/220px-Mickey-Mouse.jpg', 'abc', b'0'),
+(5, 'Hùng', '0977227990', 'Vũng Tàu', b'1', '1991-02-14', 'yellow@gmail.com', 'https://bigcedar.com/wp-content/uploads/2020/12/AdobeStock_453941794-1.jpg', 'abc', b'0'),
+(6, 'Hương', '0933445566', 'Lâm Đồng', b'0', '1977-06-05', 'pink@gmail.com', 'https://static.wikia.nocookie.net/disney/images/a/a3/Minnie_Mouse_transparent.png/revision/latest?cb=20210124014343', 'abc', b'1'),
+(8, 'Bùi Xuân Bảo', '0988667981', '338 Hai Bà Trưng, Phường 8, 1, Thành phố Hồ Chí Minh', b'1', '1987-03-20', 'green@gmail.com', 'https://cafebiz.cafebizcdn.vn/thumb_w/600/2019/photo1557224364584-1557224364877-crop-15572243735951912688033.jpg', 'abc', b'1'),
+(9, 'Đặng Thanh Phương', '0979667790', '285 Hai Bà Trưng, Phường 8, Quận 3, Thành phố Hồ Chí Minh', b'0', '1992-07-08', 'teal@gmail.com', 'https://pbs.twimg.com/media/ExMLCgWVoAQaBef?format=jpg&name=large', 'abc', b'0');
 
 -- --------------------------------------------------------
 
@@ -54,8 +62,9 @@ INSERT INTO `adm_list` (`id`, `name`, `phone`, `address`, `gender`, `birthday`, 
 -- Table structure for table `cli_list`
 --
 
-CREATE TABLE `cli_list` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `cli_list`;
+CREATE TABLE IF NOT EXISTS `cli_list` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `gender` bit(2) NOT NULL,
   `address` varchar(200) DEFAULT NULL,
@@ -64,8 +73,9 @@ CREATE TABLE `cli_list` (
   `photo` varchar(200) DEFAULT NULL,
   `password` varchar(200) NOT NULL,
   `birthday` date DEFAULT NULL,
-  `token` varchar(200) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `token` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `cli_list`
@@ -82,14 +92,16 @@ INSERT INTO `cli_list` (`id`, `name`, `gender`, `address`, `email`, `phone`, `ph
 -- Table structure for table `manufactures`
 --
 
-CREATE TABLE `manufactures` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `manufactures`;
+CREATE TABLE IF NOT EXISTS `manufactures` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `description` text NOT NULL,
   `phone` varchar(15) NOT NULL,
   `photo` varchar(200) NOT NULL,
-  `address` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `address` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `manufactures`
@@ -98,7 +110,8 @@ CREATE TABLE `manufactures` (
 INSERT INTO `manufactures` (`id`, `name`, `description`, `phone`, `photo`, `address`) VALUES
 (1, 'CONVERSE', '', '123', '', 'here'),
 (3, 'Bitis', '', '', '', ''),
-(4, 'Nike', '', '', '', '');
+(4, 'Nike', '', '', '', ''),
+(5, 'Adidas', 'Adidas là một nhà sản xuất dụng cụ thể thao của Đức, một thành viên của Adidas Group.', '0917221177', 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Adidas_Logo.svg/800px-Adidas_Logo.svg.png', '1 17A ST, Aeon Mall, Aeon Binh Tan, 700000, Ho Chi Minh, Việt Nam');
 
 -- --------------------------------------------------------
 
@@ -106,14 +119,17 @@ INSERT INTO `manufactures` (`id`, `name`, `description`, `phone`, `photo`, `addr
 -- Table structure for table `out_list`
 --
 
-CREATE TABLE `out_list` (
+DROP TABLE IF EXISTS `out_list`;
+CREATE TABLE IF NOT EXISTS `out_list` (
   `id` int(11) NOT NULL,
   `client_id` int(11) NOT NULL,
   `order_time` timestamp NOT NULL,
   `receiver_name` varchar(50) NOT NULL,
   `receiver_phone` varchar(15) NOT NULL,
   `receiver_address` varchar(100) NOT NULL,
-  `note` enum('hủy','mới','duyệt') NOT NULL
+  `note` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_out_manufacturers_id` (`client_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -130,11 +146,23 @@ INSERT INTO `out_list` (`id`, `client_id`, `order_time`, `receiver_name`, `recei
 -- Table structure for table `out_product`
 --
 
-CREATE TABLE `out_product` (
+DROP TABLE IF EXISTS `out_product`;
+CREATE TABLE IF NOT EXISTS `out_product` (
   `out_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL
+  `quantity` int(11) NOT NULL,
+  PRIMARY KEY (`out_id`,`product_id`),
+  KEY `FK_product_id` (`product_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `out_product`
+--
+
+INSERT INTO `out_product` (`out_id`, `product_id`, `quantity`) VALUES
+(1, 5, 2),
+(1, 6, 12),
+(2, 12, 6);
 
 -- --------------------------------------------------------
 
@@ -142,10 +170,12 @@ CREATE TABLE `out_product` (
 -- Table structure for table `products_category`
 --
 
-CREATE TABLE `products_category` (
-  `id` int(11) NOT NULL,
-  `category` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+DROP TABLE IF EXISTS `products_category`;
+CREATE TABLE IF NOT EXISTS `products_category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `products_category`
@@ -162,10 +192,12 @@ INSERT INTO `products_category` (`id`, `category`) VALUES
 -- Table structure for table `products_gender`
 --
 
-CREATE TABLE `products_gender` (
-  `id` tinyint(4) NOT NULL,
-  `gender` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+DROP TABLE IF EXISTS `products_gender`;
+CREATE TABLE IF NOT EXISTS `products_gender` (
+  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `gender` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `products_gender`
@@ -183,8 +215,9 @@ INSERT INTO `products_gender` (`id`, `gender`) VALUES
 -- Table structure for table `products_list`
 --
 
-CREATE TABLE `products_list` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `products_list`;
+CREATE TABLE IF NOT EXISTS `products_list` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(200) NOT NULL,
   `price` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
@@ -192,15 +225,19 @@ CREATE TABLE `products_list` (
   `category_id` int(11) NOT NULL,
   `description` text NOT NULL,
   `photo` varchar(200) NOT NULL,
-  `manufacturers_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `manufacturers_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_products_manufacturers_id` (`manufacturers_id`),
+  KEY `FK_products_gender` (`gender_id`) USING BTREE,
+  KEY `FK_products_category` (`category_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `products_list`
 --
 
 INSERT INTO `products_list` (`id`, `name`, `price`, `quantity`, `gender_id`, `category_id`, `description`, `photo`, `manufacturers_id`) VALUES
-(2, 'Run Star Hike Create Next Purpose: Sustainability Hi-Top\r\n', 2500000, 1000, 3, 3, '', 'https://www.converse.com.vn/pictures/catalog/products/sneakers/2021/ctas/171575/171575Cstandard.jpg', 1),
+(2, 'Run Star Hike Create Next Purpose: Sustainability Hi-Top', 2500000, 1000, 3, 3, '', 'https://www.converse.com.vn/pictures/catalog/products/sneakers/2021/ctas/171575/171575Cstandard.jpg', 1),
 (5, 'Chuck Taylor All Star Lift Surface Fusion', 1500000, 1000, 3, 3, '', 'https://www.converse.com.vn/pictures/catalog/products/sneakers/2021/ctas/571670c/571670Cstandard.jpg', 1),
 (6, 'Chuck Taylor All Star Crater Knit High Top', 1500000, 1000, 4, 3, '', 'https://www.converse.com.vn/pictures/catalog/products/sneakers/2021/ctas/170869c/170869Cshot2.jpg', 1),
 (7, 'Giày sandal nữ Aokang 682831037', 150000, 20, 2, 1, '', 'https://img.yes24.vn/Upload/ProductImage/anvietsh/2013660_L.jpg?width=550&height=550', 4),
@@ -210,8 +247,10 @@ INSERT INTO `products_list` (`id`, `name`, `price`, `quantity`, `gender_id`, `ca
 (11, 'Giày Sandal Nam Aokang 182831014', 170000, 202, 1, 1, '', 'https://shop-cdn.coccoc.com/images/product/Hg/Cf/HgCfycRike.jpg', 4),
 (12, 'Giày Sandal Doctor Nam BIGGBEN Da Bò Thật SD70', 200000, 999, 1, 1, '', 'https://salt.tikicdn.com/cache/400x400/ts/product/74/a2/d3/f02e8c1714cf99b458cba8269bb6d232.jpg.webp', 1),
 (13, 'Sandal Eva Phun Nam Biti\'s Hunter DEMH00201XAM (Xám)', 1500000, 1000, 1, 1, '', 'https://salt.tikicdn.com/cache/400x400/ts/product/d5/1f/b6/5fb3c22b35097a46a7f46f436bbbc2e3.jpg.webp', 3),
-(14, 'Giày Thể Thao Cao Cấp Nữ Biti\'s Hunter Layered Upper DSWH02800HOG (Hồng)', 15000009, 999, 2, 3, '', 'https://product.hstatic.net/1000230642/product/02800hog__2__dd6e1a064a294e108c2c90e7a728470d_1024x1024.jpg', 3),
-(15, 'Giày Tây Nam Biti\'s DVM283770NAD (Nâu)', 150000, 999, 1, 2, '', 'https://product.hstatic.net/1000230642/product/dvm283770nad_dd6947c86c3049a99227e2854ddc1ef0_1024x1024.jpg', 3);
+(14, 'Giày Thể Thao Cao Cấp Nữ Biti', 15000009, 998, 2, 3, '', 'https://product.hstatic.net/1000230642/product/02800hog__2__dd6e1a064a294e108c2c90e7a728470d_1024x1024.jpg', 3),
+(15, 'Giày Tây Nam Biti\'s DVM283770NAD (Nâu)', 150000, 999, 1, 2, '', 'https://product.hstatic.net/1000230642/product/dvm283770nad_dd6947c86c3049a99227e2854ddc1ef0_1024x1024.jpg', 3),
+(16, 'ZX 10000 KRUSTY BURGER', 3100000, 1000, 3, 3, 'Giày thể thao năng động đầy màu sắc', 'https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/c59ed45d8b8c443481fcac7c01403b85_9366/ZX_10000_KRUSTY_BURGER_trang_H05783_01_standard.jpg', 5),
+(17, 'Giày Superstar 82', 3500000, 999, 1, 3, 'Giày thể thao sành điệu chất chơi', 'https://assets.adidas.com/images/w_276,h_276,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/8c3a61df8aea41d48b16ad6500b34711_9366/Giay_Superstar_82_DJen_GX3746_01_standard.jpg', 5);
 
 -- --------------------------------------------------------
 
@@ -219,122 +258,23 @@ INSERT INTO `products_list` (`id`, `name`, `price`, `quantity`, `gender_id`, `ca
 -- Table structure for table `receipt_history`
 --
 
-CREATE TABLE `receipt_history` (
+DROP TABLE IF EXISTS `receipt_history`;
+CREATE TABLE IF NOT EXISTS `receipt_history` (
   `out_id` int(11) NOT NULL,
-  `adm_id` int(11) NOT NULL,
-  `receipt_stat` tinyint(4) NOT NULL,
-  `work_time` timestamp NOT NULL
+  `adm_id` int(11) DEFAULT NULL,
+  `receipt_stat` enum('Đã hủy','Mới','Đã duyệt') NOT NULL,
+  `work_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`out_id`),
+  KEY `FK_out_adm_id` (`adm_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Indexes for dumped tables
+-- Dumping data for table `receipt_history`
 --
 
---
--- Indexes for table `adm_list`
---
-ALTER TABLE `adm_list`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `cli_list`
---
-ALTER TABLE `cli_list`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `manufactures`
---
-ALTER TABLE `manufactures`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `out_list`
---
-ALTER TABLE `out_list`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FK_out_manufacturers_id` (`client_id`);
-
---
--- Indexes for table `out_product`
---
-ALTER TABLE `out_product`
-  ADD PRIMARY KEY (`out_id`,`product_id`),
-  ADD KEY `FK_product_id` (`product_id`);
-
---
--- Indexes for table `products_category`
---
-ALTER TABLE `products_category`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `products_gender`
---
-ALTER TABLE `products_gender`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `products_list`
---
-ALTER TABLE `products_list`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FK_products_manufacturers_id` (`manufacturers_id`),
-  ADD KEY `FK_products_gender` (`gender_id`) USING BTREE,
-  ADD KEY `FK_products_category` (`category_id`) USING BTREE;
-
---
--- Indexes for table `receipt_history`
---
-ALTER TABLE `receipt_history`
-  ADD PRIMARY KEY (`out_id`,`adm_id`),
-  ADD KEY `FK_admin_id` (`adm_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `adm_list`
---
-ALTER TABLE `adm_list`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `cli_list`
---
-ALTER TABLE `cli_list`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
-
---
--- AUTO_INCREMENT for table `manufactures`
---
-ALTER TABLE `manufactures`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `out_list`
---
-ALTER TABLE `out_list`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `products_category`
---
-ALTER TABLE `products_category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `products_gender`
---
-ALTER TABLE `products_gender`
-  MODIFY `id` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `products_list`
---
-ALTER TABLE `products_list`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+INSERT INTO `receipt_history` (`out_id`, `adm_id`, `receipt_stat`, `work_time`) VALUES
+(1, 3, 'Đã duyệt', '2021-12-29 19:47:17'),
+(2, 3, 'Đã duyệt', '2022-01-12 04:25:18');
 
 --
 -- Constraints for dumped tables
@@ -350,8 +290,8 @@ ALTER TABLE `out_list`
 -- Constraints for table `out_product`
 --
 ALTER TABLE `out_product`
-  ADD CONSTRAINT `FK_product_id` FOREIGN KEY (`product_id`) REFERENCES `products_list` (`id`),
-  ADD CONSTRAINT `FK_product_receipt_id` FOREIGN KEY (`out_id`) REFERENCES `out_list` (`id`);
+  ADD CONSTRAINT `FK_out_list` FOREIGN KEY (`out_id`) REFERENCES `out_list` (`id`),
+  ADD CONSTRAINT `FK_product_id` FOREIGN KEY (`product_id`) REFERENCES `products_list` (`id`);
 
 --
 -- Constraints for table `products_list`
@@ -365,8 +305,8 @@ ALTER TABLE `products_list`
 -- Constraints for table `receipt_history`
 --
 ALTER TABLE `receipt_history`
-  ADD CONSTRAINT `FK_admin_id` FOREIGN KEY (`adm_id`) REFERENCES `adm_list` (`id`),
-  ADD CONSTRAINT `FK_out_history_id` FOREIGN KEY (`out_id`) REFERENCES `out_list` (`id`);
+  ADD CONSTRAINT `FK_out_adm_id` FOREIGN KEY (`adm_id`) REFERENCES `adm_list` (`id`),
+  ADD CONSTRAINT `FK_out_list_id` FOREIGN KEY (`out_id`) REFERENCES `out_list` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- do_an/admin/super_admin_view.php
+++
@@ -1,6 +0,0 @@
-				case 'manufactures':
-				xhttp.open("GET","detail_view/manufacturers_detail.php?d=" + num);
-				break;
-				case 'admin':
-				xhttp.open("GET","detail_view/admin_detail.php?d=" + num);
-				break;
