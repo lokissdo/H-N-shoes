564aa37e570d0b3e34eaffc5b496570f4ac1d244
HurdyGutty <gaming.acount.1996@gmail.com>
2022-02-08 08:34:49 +0700

    Thêm list product trong manufactures

--- do_an/admin/detail_view/manufacturers_detail.php
+++ do_an/admin/detail_view/manufacturers_detail.php
@@ -15,10 +15,24 @@ echo 				"<button onclick=\"fix_page(".$id.")\">Sửa thông tin</button>";
 echo 		"</div>";
 echo 	"</div>";
 
-	$qry_list = "select * from out_product left join products_list on out_product.product_id = products_list.id left join manufactures on products_list.manufacturers_id = manufactures.id left join out_list on out_list.id = out_product.out_id where manufactures.id = 1 && datediff(NOW(),out_list.order_time) >= 0 && datediff(NOW(),out_list.order_time) <= ?;";
+	$qry_list = "select t3.product_id, t3.name, COALESCE(t1.number, 0) AS number_week, COALESCE(t2.number, 0) AS number_month
+					from
+						(select out_product.product_id,products_list.name,count(out_product.product_id) as number from out_product left join products_list on out_product.product_id = products_list.id left join manufactures on products_list.manufacturers_id = manufactures.id left join out_list on out_list.id = out_product.out_id where manufactures.id = '$id' && datediff(NOW(),out_list.order_time) >= 0 && datediff(NOW(),out_list.order_time) <= ? group by out_product.product_id) t1 
+					right join
+						(select out_product.product_id,products_list.name,count(out_product.product_id) as number from out_product left join products_list on out_product.product_id = products_list.id left join manufactures on products_list.manufacturers_id = manufactures.id left join out_list on out_list.id = out_product.out_id where manufactures.id = '$id' && datediff(NOW(),out_list.order_time) >= 0 && datediff(NOW(),out_list.order_time) <= ? group by out_product.product_id) t2
+					on (t1.product_id = t2.product_id)
+					right join
+						(select products_list.id as product_id,products_list.name from out_product right join products_list on out_product.product_id = products_list.id right join manufactures on products_list.manufacturers_id = manufactures.id where manufactures.id = '$id') t3
+					on (t3.product_id = t2.product_id)
+					ORDER BY number_month DESC
+					;";
 	$stmt = $ket_noi->prepare($qry_list);
-	$stmt->bind_param("i", 30);
-	$stmt
+	$week = 7;
+	$month = 30;
+	$stmt->bind_param("ii", $week, $month);
+	$stmt->execute();
+	$result_stmt = $stmt->get_result();
+	$data = $result_stmt->fetch_all(MYSQLI_ASSOC);
 
 echo 	"<div id='manufactures_right_detail_panel'>";
 echo 		"<div id='manufactures_info_detail_products_list'>";
@@ -29,7 +43,17 @@ echo 			"<div class='manufactures_info_detail_products_list_rows'>
 					<div class=\"align_center\">Tên sản phẩm</div>
 					<div class=\"align_center\">Tổng số sản phẩm theo tuần</div>
 					<div class=\"align_center\">Tổng số sản phẩm theo tháng</div>
-				</div>";				
+				</div>";
+echo 			"<div class='manufactures_info_detail_products_list_content'>";
+				foreach ($data as $row) {
+			
+echo 			"<div class='manufactures_info_detail_products_list_rows'>
+					<div class=\"align_center\">".$row['name']."</div>
+					<div class=\"align_center\">".$row['number_week']."</div>
+					<div class=\"align_center\">".$row['number_month']."</div>
+				</div>";
+				}
+		// }		
 echo		"</div>";
 echo 		"<div></div>";
 echo 		"<div></div>";
